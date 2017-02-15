89ab89a72fb5bb202e94cafb758df2517bb1d4e3
Ben Mishkin <bmishkin@mac.com>
2017-02-15 16:10:40 -0600

    use rubygems 2.6.10 and bundler 1.13.16

--- scripts/scoutrfp-gems.sh
+++ scripts/scoutrfp-gems.sh
@@ -3,8 +3,9 @@
     (cat <<'EOF'
 rvm use 2.1.10@global --default && \
 gem uninstall bundler &&  \
-gem install bundler -v '1.12.5' && \
+gem install bundler -v '1.13.6' && \
 rvm use 2.1.10@scout --default && \
+gem update --system 2.6.10 && \
 bundler --version
 EOF
 ) | sudo -H -i -u ubuntu
